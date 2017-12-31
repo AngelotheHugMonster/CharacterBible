dc25a5a63cbb27c3d3788e5cfca97fa941a08153
AngeloTheHugMonster <aarriaga1003@gmail.com>
2017-12-30 21:41:09 -0800

    Just messing around

--- myLocalRepository/FilePractice/src/Main.java
+++ myLocalRepository/FilePractice/src/Main.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
 
 public class Main
 {
-
+	private static Scanner input = new Scanner(System.in);
+	
 	public static void main(String[] args) 
 	{
-		//initial
+		System.out.println("Enter a file name: ");
+		String name = input.next();
+		System.out.println("Enter a description: ");
+		String desc = input.nextLine();
+		
+		String path = "E:\\test\\" + name + ".txt";
 	}
 
 }
